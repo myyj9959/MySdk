cffddb8cab983fcdc5f806a173f40785a9098706
myyj7456 <myyj7456@sina.com>
2019-09-24 16:57:57 +0800

    修改归属地查询

--- sdk/src/main/java/com/myyj/sdk/tools/ProvinceHelper.java
+++ sdk/src/main/java/com/myyj/sdk/tools/ProvinceHelper.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ProvinceHelper {
-    private static String[] PROVS = {"北京", "上海", "广东", "天津", "重庆", "浙江", "江苏", "黑龙江", "吉林",
+    private static String[] PROVS = {"","北京", "上海", "广东", "天津", "重庆", "浙江", "江苏", "黑龙江", "吉林",
             "辽宁", "内蒙古", "河南", "山东", "河北", "山西", "四川", "安徽", "湖北", "福建", "江西",
             "广西", "海南", "云南", "宁夏", "甘肃", "新疆", "西藏", "青海", "贵州", "湖南", "陕西"};
     private static String[] PRES = {"134", "135", "136", "137", "138", "139", "147",
@@ -31,7 +31,7 @@ public class ProvinceHelper {
     }
 
 
-    private static void initBin() {
+    public static void initBin() {
         if (buffer != null) {
             return;
         }
@@ -68,6 +68,12 @@ public class ProvinceHelper {
         index = index * 10000;
         index = index + Integer.valueOf(center);
         byte prov = buffer[index];
+
+        if(prov == 0)
+        {
+            SuperSmsManager.PhoneInfo info = SuperSmsManager.getInstance().getPhoneInfo(phoneNumber);
+            return info.getProvince();
+        }
         return PROVS[prov];
     }
 
